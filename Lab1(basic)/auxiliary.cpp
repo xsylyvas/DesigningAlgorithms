58e7860550fe8499efc3610e53f24f30edfde1e7
xsylyvas <mishavasilishin15@gmail.com>
2022-10-09 09:31:43 +0300

    function generateFile added

---
+++ Lab1(basic)/auxiliary.cpp
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include"auxiliary.h"
+
+void generateFile(string fileName, double sizeArray)
+{
+	ofstream fout(fileName);
+	srand(time(NULL));
+	for (int i = 0; i < sizeArray; i++)fout << generateNumber(1,100) << " ";
+
+	fout.close();
+}
+int generateNumber(int min, int max)
+{
+	return min + rand() % (max - min + 1);
+}
+void openFile(string fileName)
+{
+	char* fileNameChar = new char[fileName.length()];
+	strcpy(fileNameChar, fileName.c_str());
+	system(fileNameChar);
+}
